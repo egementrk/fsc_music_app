aa3fc3f82dac501f954f8474a311e47b6cf22801
egementrk <turk.egemen@hotmail.com>
2021-09-04 09:59:33 +0300

    routing done

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:fsc_music_app/components/home_page.dart';
+import 'package:fsc_music_app/components/onboarding/onboarding_main.dart';
+import 'package:fsc_music_app/components/splash_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: ,);
+    return MaterialApp(
+      initialRoute: "/",
+      routes: {
+        "/": (context) => SplashScreen(),
+        "/onboard": (context) => OnboardingMain(),
+        "/main": (context) => HomePage(),
+      },
+      // home: SplashScreen(),
+    );
   }
 }
